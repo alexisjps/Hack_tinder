ce3b690b2fabd2993af61809509cc9a058de6c17
Stephan Alexis <62907454+alexisjps@users.noreply.github.com>
2020-12-05 21:13:58 +0100

    Update Hack_tinder.js

--- Hack_tinder.js
+++ Hack_tinder.js
@@ -32,8 +32,7 @@ function hasValidProfile() {
     console.log(bio);
     return !hasBlacklistKeywords(bio);
   } catch (e) {
-    // console.log(e);
-    return true; // possible empty bio
+    return true; // possible bio vide 
   }
 }
 
@@ -80,15 +79,9 @@ function trickTinder() {
     dislike.click();
   }
 
-  // If reached max likes per day then show modal and get it's content...
-  // Check if there is any subscription button...
   if (document.getElementsByClassName('productButton__subscriptionButton').length > 0) {
-    // We get the counter thing
     const hms = document.getElementsByClassName('Fz($ml)')[0].textContent;
-    // Split it at the colons
     const a = hms.split(':');
-    // Minutes are worth 60 seconds. Hours are worth 60 minutes. 1 second = 1kmilliseconds.
-    // Genius... rocket science...
     const seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2])
 
     return seconds * 1000;
@@ -99,22 +92,14 @@ function checkOkCupid() {
   return window.location.href.startsWith("https://www.okcupid.com/doubletake");
 }
 function trickOkCupid() {
-  // Press the like button
   document.getElementsByClassName('cardactions-action--like')[0].click();
 }
-
-// There is a lot more fun that can be achieved
-// Need to add socket puppetry (VPNs solutions? several accounts?) - :D
-// TODO: Need to accept automatically permissions except for
-// TODO: Need to add ANN for fake pics
-// TODO: Need to add RNN for fake messages
 
 function getRandomPeriod() {
   return Math.round(Math.random() * (2000 - 500)) + 500;
 }
 
 (function loopSasori() {
-  // A random period between 500ms and 2secs
   let randomPeriod = getRandomPeriod();
 
   setTimeout(function () {
